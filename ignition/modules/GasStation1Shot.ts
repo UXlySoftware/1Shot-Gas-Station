f658e20aa2a5599462216d0df4c90285cbf7c486
TtheBC01 <tac688@alumni.stanford.edu>
2025-07-29 22:28:43 -0700

    removed boilerplate examples

---
+++ ignition/modules/GasStation1Shot.ts
@@ -0,0 +1,15 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const LIFI_DIAMOND = 0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE;
+
+const GasStationModule = buildModule("GasStation", (m) => {
+
+  const gasStation = m.contract("GasStation1Shot", [LIFI_DIAMOND]);
+
+  return { gasStation };
+});
+
+export default GasStationModule;
